4e59ffd952c573c49e8f673a1266253c0422da3e
Ashu Singh <ashu.140oct@gmail.com>
2024-05-12 13:08:14 +0530

    clean code

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import router from "./routes";
+
+const PORT = 3000;
+const app = express();
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+});
