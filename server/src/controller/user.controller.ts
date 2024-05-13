9c7ab8a85eee48bb156cb87b97d3bddbf07739fd
Ashu Singh <ashu.140oct@gmail.com>
2024-05-13 12:57:32 +0530

    add user sign in and jwt

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -10,7 +10,10 @@ export const signup = async (req: Request, res: Response) => {
         if(!name || !email || !password){
             return res.status(400).json({message: "Provide all the details"});
         }
-
+        const checkUser = await User.findOne({email: email});
+        if(checkUser){
+            return res.status(400).json({message: "User with this email alread exist, Try another email"});
+        }
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await User.create({
@@ -28,6 +31,34 @@ export const signup = async (req: Request, res: Response) => {
         return res.status(201).json({message: "sign up successfully", newUser});
     } catch (error) {
         console.log("Something went wrong while sign up", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+}
+
+// user sign-in
+export const signIn = async (req: Request, res:Response) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "Please provide all the details."});
+        }
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.status(404).json({message: "User with this email not found. Try sign up"});
+        }
+        const checkPassword = bcrypt.compare(user.password, password);
+        if(!checkPassword){
+            return res.status(400).json({message: "Wrong password or email"});
+        }
+
+        console.log(process.env.JWT_KEY);
+        
+        const token = jwt.sign({_id: user._id}, process.env.JWT_KEY || '');
+
+        return res.status(200).json({message: "Signed in successfully",token});
+        
+    } catch (error) {
+        console.log("Something went wrong while sign in", error);
         return res.status(500).json({message: "Internal server error"});
     }
 }
