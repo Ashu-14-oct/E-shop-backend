c603ab7f315e26ba3d1de150dc8aba6c6fff0568
Ashu Singh <ashu.140oct@gmail.com>
2024-05-14 17:25:13 +0530

    add sign up page

--- client/src/_auth/forms/SignupForm.tsx
+++ client/src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignupValidation } from "@/lib/validation";
+import { z } from "zod";
 
 const SignupForm = () => {
-  return (
-    <div>SignupForm</div>
-  )
-}
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignupForm
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col ">
+        <img src="/assets/images/logo.jpg" className="w-20 h-30" alt="Logo" />
+
+        <h2 className="h3-hold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        {/* <p className="text-light-3 small-medium md:base-regular mt-2">
+          Enter you details.
+        </p> */}
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Enter you name" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Enter you email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Create a password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">Submit</Button>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignupForm;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        lightGray: '#D3D3D3',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
