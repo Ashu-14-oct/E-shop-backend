30bc30b793bf12120ae87128cc77021eab768e12
Ashu Singh <ashu.140oct@gmail.com>
2024-05-15 22:41:02 +0530

    clean code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import express from "express";
 import router from "./routes";
 import "./config/mongoose";
@@ -8,6 +9,7 @@ import userR from "./routes/userRoute";
 const PORT = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/product', router);
